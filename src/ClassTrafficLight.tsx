5754ced1494b6de513c32872c6360e0083e4bbd9
Daniel <dragondsg@gmail.com>
2023-10-09 21:49:54 -0400

    done

--- src/ClassTrafficLight.tsx
+++ src/ClassTrafficLight.tsx
@@ -1,17 +1,39 @@
 import { Component } from "react";
 
 export class ClassTrafficLight extends Component {
+  state = {
+    count: 0
+  };
+
+  increment = () => {
+    this.setState({count: this.state.count + 1});
+    if (this.state.count >= 2) {
+      this.setState({count: 0});
+    }
+  }
+
   render() {
+    let redLight = 'black';
+    let yellowLight = 'black';
+    let greenLight = 'black';
+    if (this.state.count == 0) {
+      redLight = 'red';
+    } else if (this.state.count == 1) {
+      greenLight = 'green';
+    } else {
+      yellowLight = 'yellow';
+    }
+
     return (
       <div className="traffic-light-box">
         <h2>Class Traffic Light</h2>
         <div className="traffic-light">
           {/* Background color can be black | yellow | red | green */}
-          <div className="circle red"></div>
-          <div className="circle black"></div>
-          <div className="circle black"></div>
+          <div className={`circle ${redLight}`}></div>
+          <div className={`circle ${yellowLight}`}></div>
+          <div className={`circle ${greenLight}`}></div>
         </div>
-        <button className="next-state-button">Next State</button>
+        <button className="next-state-button" onClick={this.increment}>Next State</button>
       </div>
     );
   }
--- src/FunctionalTrafficLight.tsx
+++ src/FunctionalTrafficLight.tsx
@@ -1,17 +1,36 @@
+import { useState } from 'react';
+
 export const FunctionalTrafficLight = () => {
+  const [count, setCount] = useState(0);
+
+  let redLight = 'black';
+  let yellowLight = 'black';
+  let greenLight = 'black';
+  if (count == 0) {
+    redLight = 'red';
+  } else if (count == 1) {
+    greenLight = 'green';
+  } else {
+    yellowLight = 'yellow';
+  }
+
+  const increment = () => {
+    setCount(count + 1);
+    if (count >= 2) {
+      setCount(0);
+    }
+  }
+
   return (
     <div className="traffic-light-box">
       <h2>Functional Traffic Light</h2>
       <div className="traffic-light">
         {/* Background color can be black | yellow | red | green */}
-        <div className="circle black"></div>
-        <div className="circle yellow"></div>
-        <div className="circle green"></div>
+        <div className={`circle ${redLight}`}></div>
+        <div className={`circle ${yellowLight}`}></div>
+        <div className={`circle ${greenLight}`}></div>
       </div>
-      <button className="next-state-button">Next State</button>
+      <button className="next-state-button" onClick={increment}>Next State</button>
     </div>
   );
 };
-
-
-
